8a3d2e181524b5bf93e2c8a74ad1d671fc6537aa
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2025-07-15 21:08:11 +0300

    docs(openapi): added contact info

--- src/main/java/com/ebektasiadis/meetingroombooking/config/OpenApiConfig.java
+++ src/main/java/com/ebektasiadis/meetingroombooking/config/OpenApiConfig.java
@@ -1,21 +1,30 @@
 package com.ebektasiadis.meetingroombooking.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(Environment environment) {
+
         return new OpenAPI()
                 .info(
                         new Info()
                                 .title("Meeting Room Booking API")
                                 .description("Create and manage users, meetings rooms and their bookings through this API.")
                                 .version("1.0")
+                                .contact(
+                                        new Contact()
+                                                .url("https://ebektasiadis.dev")
+                                                .name("Efstratios Bektasiadis")
+                                                .email("ebektasiadis@gmail.com")
+                                )
                 );
     }
 }
