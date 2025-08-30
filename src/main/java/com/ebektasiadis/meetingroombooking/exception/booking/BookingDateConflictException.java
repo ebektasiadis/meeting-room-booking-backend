aefbb3309ff0f3145cfb92020b4ded3c2ff2c971
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2025-08-30 18:50:57 +0300

    feat(exceptions): moved status and extensions inside ResponseProblemDetail

--- src/main/java/com/ebektasiadis/meetingroombooking/exception/booking/BookingDateConflictException.java
+++ src/main/java/com/ebektasiadis/meetingroombooking/exception/booking/BookingDateConflictException.java
@@ -1,17 +1,23 @@
 package com.ebektasiadis.meetingroombooking.exception.booking;
 
 import com.ebektasiadis.meetingroombooking.exception.common.AbstractApiException;
+import com.ebektasiadis.meetingroombooking.exception.common.Extension;
 import com.ebektasiadis.meetingroombooking.exception.common.ResponseProblemDetail;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-@ResponseStatus(HttpStatus.CONFLICT)
-@ResponseProblemDetail(type = "booking-date-conflict", title = "Meeting room is already booked for the requested time slot.")
+@ResponseProblemDetail(
+        status = HttpStatus.CONFLICT,
+        type = "booking-date-conflict",
+        title = "Meeting room is already booked for the requested time slot.",
+        extensions = {
+                @Extension(name = "meetingRoomId", type = Long.class),
+        }
+)
 public class BookingDateConflictException extends AbstractApiException {
     private final Long meetingRoomId;
 
--- src/main/java/com/ebektasiadis/meetingroombooking/exception/meetingroom/MeetingRoomNotFoundException.java
+++ src/main/java/com/ebektasiadis/meetingroombooking/exception/meetingroom/MeetingRoomNotFoundException.java
@@ -1,17 +1,23 @@
 package com.ebektasiadis.meetingroombooking.exception.meetingroom;
 
 import com.ebektasiadis.meetingroombooking.exception.common.AbstractApiException;
+import com.ebektasiadis.meetingroombooking.exception.common.Extension;
 import com.ebektasiadis.meetingroombooking.exception.common.ResponseProblemDetail;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-@ResponseStatus(HttpStatus.NOT_FOUND)
-@ResponseProblemDetail(type = "meeting-room-not-found", title = "Meeting Room not found.")
+@ResponseProblemDetail(
+        status = HttpStatus.NOT_FOUND,
+        type = "meeting-room-not-found",
+        title = "Meeting Room not found.",
+        extensions = {
+                @Extension(name = "meetingRoomId", type = Long.class),
+        }
+)
 public class MeetingRoomNotFoundException extends AbstractApiException {
     private final Long meetingRoomId;
 
